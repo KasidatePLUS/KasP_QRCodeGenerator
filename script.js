8d5302dd20f97c6ca2e99464d8286a761185a471
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-05-03 10:16:25 +0700

    navmenu 1.0.1

--- script.js
+++ script.js
@@ -51,3 +51,13 @@ function generate(user_input) {
     }, 300);
   }
 }
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
